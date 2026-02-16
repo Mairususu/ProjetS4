2d72104764020d749b7a12afa3adab3aea9202fb
Yannis Ladron <yannis2.ladron@gmail.com>
2026-02-16 11:28:52 +0100

    Add of the hidden info of character

---
+++ Assets/Script/Tower.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    public float cost;
+    [SerializeField] private float nb_projectiles;
+    [SerializeField] private float shoot_per_second;
+    [SerializeField] private float projectile_speed;
+    [SerializeField] private float projectile_damage;
+    [SerializeField] private float range;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
